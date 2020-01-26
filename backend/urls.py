73ded15436111a24f3cad0800525b83e3bf0e75e
Niwgoat <nimafathinimafathi@gmail.com>
2020-01-26 12:32:02 +0330

    update settings.py

--- backend/urls.py
+++ backend/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include 
+from django.urls import path, include
+from django.conf.urls import url 
 
 
 urlpatterns = [
     path('raeis/', admin.site.urls),
+    path('', include('UserProfile.urls')),
     url(r'^api-auth/', include('rest_framework.urls'), name='rest_framework'),
 ]
