150e23f0c583989df6943afb65223fb0cb33f77d
Niwgoat <nimafathinimafathi@gmail.com>
2020-01-26 22:18:38 +0330

    correct urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,21 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
 from django.conf.urls import url 
 
 urlpatterns = [
     path('raeis/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls'), name='rest_framework'),
+    path('UserProfile/', include('UserProfile.urls')),
     path('post/', include('Post.urls'))
 ]
-=======
-from django.conf.urls import url
-import UserProfile
-
-urlpatterns = [
-    path('raeis/', admin.site.urls),
-    path('UserProfile/', include('UserProfile.urls')),
-    url(r'^api-auth/', include('rest_framework.urls'), name='rest_framework'),
-]
->>>>>>> 6c8743a27a10370dbf787df87ae1e92f517f3059
