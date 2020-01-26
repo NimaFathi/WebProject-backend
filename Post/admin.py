021caa75545d1646049328f6c4a63445d2619c22
Niwgoat <nimafathinimafathi@gmail.com>
2020-01-26 22:10:46 +0330

    add and nearly finished post

--- Post/admin.py
+++ Post/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Card, Comment
+
+
 
 # Register your models here.
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
--- backend/urls.py
+++ backend/urls.py
@@ -17,9 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url 
 
-
 urlpatterns = [
     path('raeis/', admin.site.urls),
-    path('', include('UserProfile.urls')),
     url(r'^api-auth/', include('rest_framework.urls'), name='rest_framework'),
+    path('post/', include('Post.urls'))
 ]
