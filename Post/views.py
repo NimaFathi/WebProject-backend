cd66b5edd84a03a6add7017d668d931196eeb632
hhosseini1377 <hhosseini@ce.sharif.edu>
2020-01-27 14:09:31 +0330

    hottest added
    hottest added

--- Post/views.py
+++ Post/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Comment, Card
 from .serializers import CommentSerializer, CardSerializer
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Count
 class CardView(viewsets.ModelViewSet):
     queryset = Card.objects.all() 
     serializer_class = CardSerializer
@@ -11,5 +13,8 @@ class CommentView(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
+class hottest(viewsets.ModelViewSet):
+    queryset = Card.objects.annotate(q_count=Count('voteUp')).order_by('-q_count')[:7]
+    serializer_class = CommentSerializer
 
 
