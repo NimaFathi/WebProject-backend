6a7226329f1798654cb191a6fcbcaf415535a6e1
Niwgoat <nimafathinimafathi@gmail.com>
2020-01-28 13:37:21 +0330

    uodated account

--- account/serializers.py
+++ account/serializers.py
@@ -4,7 +4,7 @@ from .models import Account
 class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ['username', 'email', 'password']
+        fields = ('username', 'email', 'password')
         extra_kwargs = {
             'password': {'write_only': True}
         }
