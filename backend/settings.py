6f8890624dd7683967106993275b104ebbe2401d
Niwgoat <nimafathinimafathi@gmail.com>
2020-01-29 00:15:07 +0330

    nearly finished account

--- backend/settings.py
+++ backend/settings.py
@@ -47,18 +47,14 @@ INSTALLED_APPS = [
     'channel',
     'account',
     'rest_framework.authtoken'
-    'django_filters'
 ]
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        
-    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
 }
 
 MIDDLEWARE = [
