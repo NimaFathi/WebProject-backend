72aed036a516260190d278b3941c30d67393a10f
Niwgoat <nimafathinimafathi@gmail.com>
2020-01-29 13:11:02 +0330

    views.py for account updated

--- account/views.py
+++ account/views.py
@@ -20,51 +20,51 @@ from rest_framework.authtoken.models import Token
 @authentication_classes([])
 def registration_view(request):
 
-	if request.method == 'POST':
-		data = {}
-		email = request.data.get('email', '0').lower()
-		if validate_email(email) != None:
-			data['error_message'] = 'That email is already in use.'
-			data['response'] = 'Error'
-			return Response(data)
+    if request.method == 'POST':
+        data = {}
+        email = request.data.get('email', '0').lower()
+        if validate_email(email) != None:
+            data['error_message'] = 'That email is already in use.'
+            data['response'] = 'Error'
+            return Response(data)
 
-		username = request.data.get('username', '0')
-		if validate_username(username) != None:
-			data['error_message'] = 'That username is already in use.'
-			data['response'] = 'Error'
-			return Response(data)
+        username = request.data.get('username', '0')
+        if validate_username(username) != None:
+            data['error_message'] = 'That username is already in use.'
+            data['response'] = 'Error'
+            return Response(data)
 
-		serializer = RegistrationSerializer(data=request.data)
-		
-		if serializer.is_valid():
-			account = serializer.save()
-			data['response'] = 'successfully registered new user.'
-			data['email'] = account.email
-			data['username'] = account.username
-			data['pk'] = account.pk
-			token = Token.objects.get(user=account).key
-			data['token'] = token
-		else:
-			data = serializer.errors
-		return Response(data)
+        serializer = RegistrationSerializer(data=request.data)
+
+        if serializer.is_valid():
+            account = serializer.save()
+            data['response'] = 'successfully registered new user.'
+            data['email'] = account.email
+            data['username'] = account.username
+            data['pk'] = account.pk
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+        else:
+            data = serializer.errors
+        return Response(data)
 
 def validate_email(email):
-	account = None
-	try:
-		account = Account.objects.get(email=email)
-	except Account.DoesNotExist:
-		return None
-	if account != None:
-		return email
+    account = None
+    try:
+        account = Account.objects.get(email=email)
+    except Account.DoesNotExist:
+        return None
+    if account != None:
+        return email
 
 def validate_username(username):
-	account = None
-	try:
-		account = Account.objects.get(username=username)
-	except Account.DoesNotExist:
-		return None
-	if account != None:
-		return username
+    account = None
+    try:
+        account = Account.objects.get(username=username)
+    except Account.DoesNotExist:
+        return None
+    if account != None:
+        return username
 
 
 # Account properties
@@ -75,14 +75,14 @@ def validate_username(username):
 @permission_classes((IsAuthenticated, ))
 def account_properties_view(request):
 
-	try:
-		account = request.user
-	except Account.DoesNotExist:
-		return Response(status=status.HTTP_404_NOT_FOUND)
+    try:
+        account = request.user
+    except Account.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-	if request.method == 'GET':
-		serializer = AccountPropertiesSerializer(account)
-		return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = AccountPropertiesSerializer(account)
+        return Response(serializer.data)
 
 
 # Account update properties
@@ -93,19 +93,19 @@ def account_properties_view(request):
 @permission_classes((IsAuthenticated, ))
 def update_account_view(request):
 
-	try:
-		account = request.user
-	except Account.DoesNotExist:
-		return Response(status=status.HTTP_404_NOT_FOUND)
-		
-	if request.method == 'PUT':
-		serializer = AccountPropertiesSerializer(account, data=request.data)
-		data = {}
-		if serializer.is_valid():
-			serializer.save()
-			data['response'] = 'Account update success'
-			return Response(data=data)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        account = request.user
+    except Account.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = AccountPropertiesSerializer(account, data=request.data)
+        data = {}
+        if serializer.is_valid():
+            serializer.save()
+            data['response'] = 'Account update success'
+            return Response(data=data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
@@ -114,29 +114,31 @@ def update_account_view(request):
 # URL: http://127.0.0.1:8000/api/account/login
 class ObtainAuthTokenView(APIView):
 
-	authentication_classes = []
-	permission_classes = []
+    authentication_classes = []
+    permission_classes = []
 
-	def post(self, request):
-		context = {}
+    def post(self, request):
+        context = {}
 
-		email = request.POST.get('username')
-		password = request.POST.get('password')
-		account = authenticate(email=email, password=password)
-		if account:
-			try:
-				token = Token.objects.get(user=account)
-			except Token.DoesNotExist:
-				token = Token.objects.create(user=account)
-			context['response'] = 'Successfully authenticated.'
-			context['pk'] = account.pk
-			context['email'] = email.lower()
-			context['token'] = token.key
-		else:
-			context['response'] = 'Error'
-			context['error_message'] = 'Invalid credentials'
+        email = request.POST.get('username')
+        password = request.POST.get('password')
+        account = authenticate(email=email, password=password)
+        if account:
+            try:
+                token = Token.objects.get(user=account)
+            except Token.DoesNotExist:
+                token = Token.objects.create(user=account)
+            context['response'] = 'Successfully authenticated.'
+            context['pk'] = account.pk
+            context['email'] = email.lower()
+            context['token'] = token.key
+        else:
+            context['hajit'] = email
+            context['hajit2'] = password
+            context['response'] = 'Error'
+            context['error_message'] = 'Invalid credentials'
 
-		return Response(context)
+        return Response(context)
 
 
 
@@ -146,47 +148,47 @@ class ObtainAuthTokenView(APIView):
 @authentication_classes([])
 def does_account_exist_view(request):
 
-	if request.method == 'GET':
-		email = request.GET['email'].lower()
-		data = {}
-		try:
-			account = Account.objects.get(email=email)
-			data['response'] = email
-		except Account.DoesNotExist:
-			data['response'] = "Account does not exist"
-		return Response(data)
+    if request.method == 'GET':
+        email = request.GET['email'].lower()
+        data = {}
+        try:
+            account = Account.objects.get(email=email)
+            data['response'] = email
+        except Account.DoesNotExist:
+            data['response'] = "Account does not exist"
+        return Response(data)
 
 
 
 class ChangePasswordView(UpdateAPIView):
 
-	serializer_class = ChangePasswordSerializer
-	model = Account
-	permission_classes = (IsAuthenticated,)
-	authentication_classes = (TokenAuthentication,)
+    serializer_class = ChangePasswordSerializer
+    model = Account
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
 
-	def get_object(self, queryset=None):
-		obj = self.request.user
-		return obj
+    def get_object(self, queryset=None):
+        obj = self.request.user
+        return obj
 
-	def update(self, request, *args, **kwargs):
-		self.object = self.get_object()
-		serializer = self.get_serializer(data=request.data)
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
 
-		if serializer.is_valid():
-			# Check old password
-			if not self.object.check_password(serializer.data.get("old_password")):
-				return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
+        if serializer.is_valid():
+            # Check old password
+            if not self.object.check_password(serializer.data.get("old_password")):
+                return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
 
-			# confirm the new passwords match
-			new_password = serializer.data.get("new_password")
-			confirm_new_password = serializer.data.get("confirm_new_password")
-			if new_password != confirm_new_password:
-				return Response({"new_password": ["New passwords must match"]}, status=status.HTTP_400_BAD_REQUEST)
+        # confirm the new passwords match
+            new_password = serializer.data.get("new_password")
+            confirm_new_password = serializer.data.get("confirm_new_password")
+            if new_password != confirm_new_password:
+                return Response({"new_password": ["New passwords must match"]}, status=status.HTTP_400_BAD_REQUEST)
 
-			# set_password also hashes the password that the user will get
-			self.object.set_password(serializer.data.get("new_password"))
-			self.object.save()
-			return Response({"response":"successfully changed password"}, status=status.HTTP_200_OK)
+    # set_password also hashes the password that the user will get
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            return Response({"response":"successfully changed password"}, status=status.HTTP_200_OK)
 
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
